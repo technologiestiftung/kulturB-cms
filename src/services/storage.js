b0df56faa02dcf164b52db259e7d4ec8653ab8e6
g-div <g-div@anche.no>
2019-02-20 16:06:40 +0100

    refactor(services): separate user and location API services

--- src/services/storage.js
+++ src/services/storage.js
@@ -4,8 +4,8 @@ const { localStoragePrefix } = config;
 
 const prefixKey = key => `${localStoragePrefix}:${key}`;
 
-const get = key => store.get(prefixKey(key));
-const set = (key, value) => store.set(prefixKey(key), value);
-const remove = key => store.remove(prefixKey(key));
+export const get = key => store.get(prefixKey(key));
+export const set = (key, value) => store.set(prefixKey(key), value);
+export const remove = key => store.remove(prefixKey(key));
 
 export default { get, set, remove };
--- src/services/userApi.js
+++ src/services/userApi.js
@@ -15,16 +15,4 @@ export async function login(values) {
   }
 }
 
-const parseQuery = (url, params) => {
-  const urlObject = new URL(url);
-  Object.keys(params).forEach(key => urlObject.searchParams.append(key, params[key]));
-  return urlObject;
-};
-
-export async function get(params) {
-  const url = parseQuery(`${config.url.base}${config.url.organisation}`, params);
-  const res = await fetch(url);
-  return res.json();
-}
-
 export default { login };
