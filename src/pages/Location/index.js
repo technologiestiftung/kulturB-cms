c918f57b7c72aaf0a4bbb3aafa78c58a6d6415b7
moklick <info@moritzklack.com>
2019-02-20 15:44:28 +0100

    feat(location): add basic crud form

--- src/pages/Location/index.js
+++ src/pages/Location/index.js
@@ -1,20 +1,108 @@
 import React, { PureComponent } from 'react';
 import Container from '~/components/Container';
+import {
+  Form, Input, Button, Spin
+} from 'antd';
+
+import { createLocation, updateLocation } from '~/services/api';
+
+const formItemLayout = {
+  labelCol: { span: 2 },
+  wrapperCol: { span: 14 }
+};
 
 class Location extends PureComponent {
-  render() {
+  state = {
+    data: null,
+    isLoading: true
+  }
+
+  componentDidMount() {
+    if (this.props.isCreateMode) {
+      return this.setState({ isLoading: false });
+    }
+
+    this.loadLocation();
+  }
+
+  onSubmit(evt) {
+    evt.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        if (this.props.isCreateMode) {
+          return createLocation(values);
+        }
+
+        updateLocation(this.props.match.params.id, values);
+      }
+    });
+  }
+
+  async loadLocation() {
     const { id } = this.props.match.params;
+    this.setState({ isLoading: true });
+
+    const res = await fetch(`${config.url.base}${config.url.organisation}/${id}`);
+    const data = await res.json();
+
+    this.setState({ data, isLoading: false });
+  }
+
+  renderForm() {
+    const {
+      getFieldDecorator
+    } = this.props.form;
+
+    return (
+      <Form onSubmit={evt => this.onSubmit(evt)} layout="horizontal">
+        <Form.Item
+          label="Name"
+          {...formItemLayout}
+        >
+          {getFieldDecorator('name', {
+            rules: [{
+              required: true, message: 'Bitte einen Namen angeben'
+            }],
+            initialValue: this.state.data.name
+          })(
+            <Input />
+          )}
+        </Form.Item>
+        <Form.Item
+          label="Webseite"
+          {...formItemLayout}
+        >
+          {getFieldDecorator('website', {
+            rules: [{
+              type: 'string', message: 'Bitten eine gültige URL angeben'
+            }],
+            initialValue: this.state.data.website
+          })(
+            <Input />
+          )}
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{ span: 12, offset: 2 }}
+        >
+          <Button type="primary" htmlType="submit">Abschicken</Button>
+        </Form.Item>
+      </Form>
+    );
+  }
+
+  render() {
     const { isCreateMode } = this.props;
+    const title = isCreateMode ? 'anlegen' : 'bearbeiten';
 
     return (
       <Container>
-        <h1>Standort {isCreateMode ? 'neu anlegen' : id}</h1>
-        <p>
-          create / edit form
-        </p>
+        <h1>Standort {title}</h1>
+        {this.state.isLoading ? <Spin /> : this.renderForm()}
       </Container>
     );
   }
 }
 
-export default Location;
+const WrappedLocation = Form.create({ name: 'location' })(Location);
+
+export default WrappedLocation;
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import fetch from 'unfetch';
 
-async function login(values) {
+export async function login(values) {
   const url = `${config.url.base}${config.url.user.base}${config.url.user.login}`;
   try {
     const res = await fetch(url, {
@@ -15,4 +15,36 @@ async function login(values) {
   }
 }
 
-export default { login };
+export async function updateLocation(id, values) {
+  const url = `${config.url.base}${config.url.organisation}/${id}`;
+  try {
+    const res = await fetch(url, {
+      method: 'PUT',
+      body: JSON.stringify(values)
+    }).then(r => r.json());
+
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function createLocation(values) {
+  const url = `${config.url.base}${config.url.organisation}`;
+  try {
+    const res = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(values)
+    }).then(r => r.json());
+
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export default {
+  login,
+  updateLocation,
+  createLocation
+};
