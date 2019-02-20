ad38877ca83e829ff6f749b7bca38b99908c8edb
moklick <info@moritzklack.com>
2019-02-20 16:06:43 +0100

    fix(location): add header

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 import fetch from 'unfetch';
 
+import Store from '~/store';
+
 export async function login(values) {
   const url = `${config.url.base}${config.url.user.base}${config.url.user.login}`;
   try {
@@ -17,10 +19,16 @@ export async function login(values) {
 
 export async function updateLocation(id, values) {
   const url = `${config.url.base}${config.url.organisation}/${id}`;
+  const { AppState } = Store.getState();
+
   try {
     const res = await fetch(url, {
       method: 'PUT',
-      body: JSON.stringify(values)
+      body: JSON.stringify(values),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: AppState.token
+      }
     }).then(r => r.json());
 
     return res;
@@ -31,10 +39,16 @@ export async function updateLocation(id, values) {
 
 export async function createLocation(values) {
   const url = `${config.url.base}${config.url.organisation}`;
+  const { AppState } = Store.getState();
+
   try {
     const res = await fetch(url, {
       method: 'POST',
-      body: JSON.stringify(values)
+      body: JSON.stringify(values),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: AppState.token
+      }
     }).then(r => r.json());
 
     return res;
@@ -47,4 +61,4 @@ export default {
   login,
   updateLocation,
   createLocation
-};
+}
