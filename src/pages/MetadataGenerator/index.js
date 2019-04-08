8cc716db8835aadcd56bef560e73918c0451fdba
g-div <g-div@anche.no>
2019-04-08 17:00:26 +0200

    fix(metadata): style select
    Former-commit-id: c494d9edcfabbdd25d526715592be7b09cd4e0d0

--- src/pages/MetadataGenerator/index.js
+++ src/pages/MetadataGenerator/index.js
@@ -74,7 +74,7 @@ class MetadataGenerator extends PureComponent {
           </p>
           <LocationSelect
             locations={this.state.locations}
-            selectLocation={(selectedLocation) => this.selectLocation(selectedLocation)}
+            selectLocation={selectedLocation => this.selectLocation(selectedLocation)}
           />
           <Divider />
           <MetadataForm
