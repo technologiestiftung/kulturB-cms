7660eb5fa4d519a59c497945220286122d086454
g-div <g-div@anche.no>
2019-04-09 16:37:23 +0200

    fix(metadata): add JSON-LD structure
    Former-commit-id: 596edd53b7973e2658dcfeabb14d331edbc4eec0

--- src/pages/MetadataGenerator/components/Code/jsonld.js
+++ src/pages/MetadataGenerator/components/Code/jsonld.js
@@ -1,25 +1,46 @@
 export default (item) => {
   const {
-    _id,
-    type,
     name,
     description,
+    logo,
     website,
     email,
-    telephone
+    telephone,
+    address,
+    zipcode,
+    city,
+    openingHours
   } = item;
 
   const res = {
     '@context': 'http://schema.org',
-    '@type': type,
-    '@id': _id,
+    '@type': 'Organization',
     name
+  };
+  const location = {
+    '@type': 'CivicStructure'
   };
 
   if (description) res.description = description;
-  if (website) res.url = res.website = res.sameAs = website;
+  if (logo && logo.url) res.logo = logo.url;
+  if (website) {
+    res.url = website;
+    res.sameAs = website;
+  }
   if (email) res.email = email;
   if (telephone) res.telephone = telephone;
+  if (address && zipcode && city) {
+    res.location = location;
 
-  return JSON.stringify(res, null, 2);
+    res.location.address = [address, zipcode, city].join(', ');
+  }
+
+  if (openingHours) {
+    if (!res.location) res.location = location;
+    res.location.openingHours = openingHours;
+  }
+
+  return `<script type="application/ld+json">
+  ${JSON.stringify(res, null, 2)}
+  </script>`;
 };
