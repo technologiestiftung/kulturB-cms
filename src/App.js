04601eb4747bbc84eec0a4ed70c190bf75935884
Christopher Möller <chrtze@gmail.com>
2019-09-04 16:43:09 +0200

    fix(router): change private route component to use render or component property
    Former-commit-id: 91df22f3feadb54f7e7e8cf489f0560b990a125d

--- src/App.js
+++ src/App.js
@@ -26,21 +26,24 @@ import submissionsApi from '~/services/submissionsApi';
 
 const { tables } = config;
 
-const PrivateRoute = ({ component: Component, token = false, ...rest }) => (
-  <Route
-    {...rest}
-    render={props => (
-      token ? <Component {...props} {...rest} token={token} /> : (
-        <Redirect
-          to={{
-            pathname: '/login',
-            state: { from: props.location }
-          }}
-        />
-      ))
-    }
-  />
-);
+const PrivateRoute = ({ component: Component, render, token = false, ...rest }) => {
+  let renderComponent = props => <Redirect to={{ pathname: '/login', state: { from: props.location }}} />;
+
+  if (token && render) {
+    renderComponent = render;
+  }
+
+  if (token && Component) {
+    renderComponent = props => <Component {...props} {...rest} token={token} />;
+  }
+
+  return (
+    <Route
+      {...rest}
+      render={renderComponent}
+    />
+  );
+};
 
 const Content = styled.div`
   padding: 0 16px;
@@ -70,19 +73,19 @@ class App extends PureComponent {
           <Header {...this.props} dispatch={this.props.dispatch} />
           <Content>
             <Switch>
-              <Route path="/" exact component={() => <Overview {...this.props} actions={api.locations} config={locations} />} />
+              <Route path="/" exact render={() => <Overview actions={api.locations} config={locations} />} />
               <Route path="/login" component={Login} />
               <Route path="/metadaten/:id" component={MetadataGenerator} />
               <Route path="/metadaten" component={MetadataGenerator} />
-              <Route path="/kulturorte/neu" exact component={() => <Details actions={api} config={{ locations }} {...this.props} isCreateMode />} />
-              <Route path="/kulturorte/:id" component={props => <Details actions={api} config={{ locations }} {...props} {...this.props} />} />
+              <Route path="/kulturorte/neu" exact render={() => <Details actions={api} config={{ locations }} {...this.props} isCreateMode />} />
+              <Route path="/kulturorte/:id" render={props => <Details actions={api} config={{ locations }} {...props} {...this.props} />} />
               <PrivateRoute token={this.props.token} path="/tags" exact component={TagsOverview} />
               <PrivateRoute {...this.props} path="/einstellungen" component={Settings} />
               <PrivateRoute token={this.props.token} path="/nutzer/neu" exact isCreateMode component={User} />
               <PrivateRoute token={this.props.token} path="/nutzer/:id" component={User} />
               <PrivateRoute {...this.props} path="/nutzer" component={UsersOverview} />
               <PrivateRoute {...this.props} path="/changes/:id" component={props => <Details actions={api} config={{ changes }} {...props} {...this.props} />} />
-              <PrivateRoute {...this.props} path="/changes" component={() => <Overview {...this.props} actions={api.changes} config={changes} />} />
+              <PrivateRoute {...this.props} path="/changes" render={() => <Overview {...this.props} actions={api.changes} config={changes} />} />
               <PrivateRoute {...this.props} path="/submissions/:id" component={props => <Details actions={api} config={{ changes }} {...props} {...this.props} />} />
               <PrivateRoute {...this.props} path="/submissions" component={() => <Overview {...this.props} actions={api.submissions} config={changes} />} />
               <PrivateRoute token={this.props.token} path="*" component={NoMatch} />
